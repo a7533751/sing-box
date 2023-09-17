829c66fbb192a69391b9a22c6fb334c13a541323
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-09-17 21:49:21 +0800

    feat: Add outbound provider support
    Start DNS transports before provider
    Add kTLS support
    Add AnyTLS link parser
    Add tls-pin-sha256 support
    Add `override_dialer` option
    Support override TLS option
    Add `path` option for remote provider
    Fix clash `tls` parser
    Fix slow startup
    
    Co-authored-by: jebbs <qjebbs@gmail.com>
    Co-authored-by: PuerNya <safarier@outlook.com>
    Co-authored-by: yelnoo <t50500411@163.com>
    Co-authored-by: reF1nd <ref1nd@thunder-cloud.cc>
    Co-authored-by: dyhkwong <50692134+dyhkwong@users.noreply.github.com>
    Co-authored-by: lux5am <lux.5am@yandex.com>
    Co-authored-by: xireiki <i@xireiki.com>

--- experimental/clashapi/server.go
+++ experimental/clashapi/server.go
@@ -45,6 +45,7 @@ type Server struct {
 	router         adapter.Router
 	dnsRouter      adapter.DNSRouter
 	outbound       adapter.OutboundManager
+	provider       adapter.ProviderManager
 	endpoint       adapter.EndpointManager
 	logger         log.Logger
 	httpServer     *http.Server
@@ -70,6 +71,7 @@ func NewServer(ctx context.Context, logFactory log.ObservableFactory, options op
 		router:    service.FromContext[adapter.Router](ctx),
 		dnsRouter: service.FromContext[adapter.DNSRouter](ctx),
 		outbound:  service.FromContext[adapter.OutboundManager](ctx),
+		provider:  service.FromContext[adapter.ProviderManager](ctx),
 		endpoint:  service.FromContext[adapter.EndpointManager](ctx),
 		logger:    logFactory.NewLogger("clash-api"),
 		httpServer: &http.Server{
@@ -118,12 +120,12 @@ func NewServer(ctx context.Context, logFactory log.ObservableFactory, options op
 		r.Get("/logs", getLogs(s.ctx, logFactory))
 		r.Get("/traffic", traffic(s.ctx, trafficManager))
 		r.Get("/version", version)
-		r.Mount("/configs", configRouter(s, logFactory))
-		r.Mount("/proxies", proxyRouter(s, s.router))
-		r.Mount("/rules", ruleRouter(s.router))
-		r.Mount("/connections", connectionRouter(s.ctx, s.router, trafficManager))
-		r.Mount("/providers/proxies", proxyProviderRouter())
-		r.Mount("/providers/rules", ruleProviderRouter())
+			r.Mount("/configs", configRouter(s, logFactory))
+			r.Mount("/proxies", proxyRouter(s, s.router))
+			r.Mount("/rules", ruleRouter(s.router))
+			r.Mount("/connections", connectionRouter(s.ctx, s.router, trafficManager))
+			r.Mount("/providers/proxies", proxyProviderRouter(s))
+			r.Mount("/providers/rules", ruleProviderRouter())
 		r.Mount("/script", scriptRouter())
 		r.Mount("/profile", profileRouter())
 		r.Mount("/cache", cacheRouter(ctx))

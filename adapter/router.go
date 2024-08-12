2633077a70ceb25832ff7cc3d4e45b611a6a28cb
PuerNya <safarier@outlook.com>
2024-08-13 05:56:16 +0800

    clash-api: Add rule provider support
    use static file instead of cache file
    makes remote rule-set `path` correct
    Support temporary disable rule

--- adapter/router.go
+++ adapter/router.go
@@ -21,9 +21,11 @@ type Router interface {
 	ConnectionRouter
 	PreMatch(metadata InboundContext) error
 	ConnectionRouterEx
+	RuleSets() []RuleSet
 	RuleSet(tag string) (RuleSet, bool)
 	NeedWIFIState() bool
 	Rules() []Rule
+	Rule(uuid string) (Rule, bool)
 	AppendTracker(tracker ConnectionTracker)
 	ResetNetwork()
 }
@@ -47,6 +49,10 @@ type ConnectionRouterEx interface {
 
 type RuleSet interface {
 	Name() string
+	Type() string
+	Format() string
+	UpdatedTime() time.Time
+	Update(ctx context.Context) error
 	StartContext(ctx context.Context, startContext *HTTPStartContext) error
 	PostStart() error
 	Metadata() RuleSetMetadata
